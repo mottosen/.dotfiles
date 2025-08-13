cef152c6fe39bce229a867aa1f8fc36b37f268fc
Marius <business@mottosen.xyz>
2025-08-13 17:44:23 +0200

    Added barebone home-manager configuration.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,18 +3,32 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
     nixosConfigurations = {
       vm = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [
           ./configuration.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+      test = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
         ];
       };
     };
