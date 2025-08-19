6c62ea508598f2f450d5fc14dcb6d7e54f9fb491
mottosen <business@mottosen.xyz>
2025-08-19 19:46:30 +0200

    lots of changes for multiple programs.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 -- Window
 config.max_fps = 120
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.macos_window_background_blur = 20 -- not sure if this is only for mac?
 config.window_padding = {
 	left = 0,
