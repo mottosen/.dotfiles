a7215209168d53724689ed56b1f4658d450c5a20
Marius <business@mottosen.xyz>
2025-09-27 22:22:09 +0200

    dont need this

--- install-scripts/install-fedora.sh
+++ install-scripts/install-fedora.sh
@@ -33,7 +33,6 @@ setup_microsoft_repo() {
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo dnf install -y https://packages.microsoft.com/config/fedora/42/packages-microsoft-prod.rpm
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    sudo dnf check-update
 }
 
 setup_devbox() {
