b879a6c952db6a8c10b5c12415848a0ce5c2c0ba
Marius <business@mottosen.xyz>
2025-09-29 15:09:59 +0200

    persistent wallpaper across reboot

--- .config/hypr/scripts/on_wallpaper_update.sh
+++ .config/hypr/scripts/on_wallpaper_update.sh
@@ -2,9 +2,11 @@
 
 wallpaper=$1
 echo "Wallpaper changed to: $wallpaper"
+rm "$HOME/wallpaper"
+ln -s "$wallpaper" "$HOME/wallpaper"
 
 echo "Wallpaper was updated, running Matugen"
-matugen image $wallpaper
+matugen image "$wallpaper"
 
 echo "Matugen colors updated, reloading waybar"
 sleep 1
