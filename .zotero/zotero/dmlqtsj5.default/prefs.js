ca78172712ad559d5a453f3767ac29f129c6cc24
Marius <business@mottosen.xyz>
2025-09-30 17:41:48 +0200

    misc

--- .zotero/zotero/dmlqtsj5.default/prefs.js
+++ .zotero/zotero/dmlqtsj5.default/prefs.js
@@ -9,11 +9,11 @@
 // - modify it via the UI (e.g. via about:config in the browser); or
 // - set it within a user.js file in your profile.
 
-user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1759097586);
-user_pref("app.update.lastUpdateTime.background-update-timer", 1759140786);
-user_pref("app.update.lastUpdateTime.xpi-signature-verification", 1759097586);
+user_pref("app.update.lastUpdateTime.addon-background-update-timer", 1759243166);
+user_pref("app.update.lastUpdateTime.background-update-timer", 1759243166);
+user_pref("app.update.lastUpdateTime.xpi-signature-verification", 1759243166);
 user_pref("browser.laterrun.bookkeeping.profileCreationTime", 1757074558);
-user_pref("browser.laterrun.bookkeeping.sessionCount", 22);
+user_pref("browser.laterrun.bookkeeping.sessionCount", 23);
 user_pref("browser.laterrun.enabled", true);
 user_pref("browser.policies.applied", true);
 user_pref("browser.startup.homepage_override.buildID", "20240801134912");
@@ -34,7 +34,7 @@ user_pref("extensions.zotero.firstRun2", false);
 user_pref("extensions.zotero.ignoreLegacyDataDir.auto", true);
 user_pref("extensions.zotero.lastCreatorFieldMode", 1);
 user_pref("extensions.zotero.lastSelectedPrefPane", "zotero-prefpane-general");
-user_pref("extensions.zotero.lastViewedFolder", "C4");
+user_pref("extensions.zotero.lastViewedFolder", "C2");
 user_pref("extensions.zotero.newItemTypeMRU", "blogPost,conferencePaper,newspaperArticle,journalArticle");
 user_pref("extensions.zotero.pane.persist", "{\"zotero-reader-sidebar-pane\":{\"collapsed\":\"false\",\"width\":\"240\"},\"zotero-layout-switcher\":{\"orient\":\"horizontal\"},\"zotero-items-splitter\":{\"orient\":\"horizontal\"},\"zotero-item-pane\":{\"width\":\"337\",\"height\":\"205\"},\"zotero-context-splitter\":{\"state\":\"\"},\"zotero-context-splitter-stacked\":{\"state\":\"open\"}}");
 user_pref("extensions.zotero.panes.abstract.open", true);
@@ -49,8 +49,6 @@ user_pref("extensions.zotero.panes.notes.open", true);
 user_pref("extensions.zotero.panes.related.open", true);
 user_pref("extensions.zotero.panes.tags.open", true);
 user_pref("extensions.zotero.prefVersion", 13);
-user_pref("extensions.zotero.purge.creators", true);
-user_pref("extensions.zotero.purge.items", true);
 user_pref("extensions.zotero.purge.tags", true);
 user_pref("extensions.zotero.reader.contentDarkMode", false);
 user_pref("extensions.zotero.sourceList.persist", "{\"L1\":true,\"C1\":true,\"P1\":false,\"C6\":true}");
@@ -59,11 +57,11 @@ user_pref("extensions.zotero.sync.reminder.setUp.lastDisplayed", 1757075031);
 user_pref("extensions.zotero.useDataDir", true);
 user_pref("extensions.zoteroOpenOfficeIntegration.lastAttemptedVersion", "7.0.5.SOURCE");
 user_pref("gecko.handlerService.defaultHandlersVersion", 1);
-user_pref("idle.lastDailyNotification", 1759138398);
+user_pref("idle.lastDailyNotification", 1759243469);
 user_pref("intl.accept_languages", "en-US, en");
 user_pref("media.gmp.storage.version.observed", 1);
 user_pref("privacy.purge_trackers.date_in_cookie_database", "0");
-user_pref("privacy.purge_trackers.last_purge", "1759138398253");
+user_pref("privacy.purge_trackers.last_purge", "1759243469509");
 user_pref("security.sandbox.content.tempDirSuffix", "99ff36fa-73be-471b-9d0c-0657b9db3be5");
 user_pref("storage.vacuum.last.content-prefs.sqlite", 1757323509);
 user_pref("storage.vacuum.last.index", 1);
