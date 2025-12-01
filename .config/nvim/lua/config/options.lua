ee6fcc3d92a51ffdbc8e0cd9db4d6159d99986b3
Marius <business@mottosen.xyz>
2025-12-01 19:29:23 +0100

    misc

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -23,7 +23,7 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
+    vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Enable break indent
