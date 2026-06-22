57aa5af1da47d6dc029990e56166f12d986e2303
Marius <business@mottosen.xyz>
2026-06-22 18:28:57 +0200

    nvim add 80 col highlight

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -72,6 +72,9 @@ vim.opt.inccommand = "split"
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
+-- Show a vertical ruler at column 80 (purely visual guide)
+vim.opt.colorcolumn = "80"
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 5
 
